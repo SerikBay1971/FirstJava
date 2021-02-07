e3085bd1f2318cd9c9ad4f035c2c895f8e1c7be9
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-07 14:54:03 +0400

    Темп. Пробники всякие.

---
+++ src/com/firstjava/temp.java
@@ -0,0 +1,13 @@
+package com.firstjava;
+
+public class temp {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+            continue;
+        }
+
+        System.out.println(2121);
+
+    }
+}
