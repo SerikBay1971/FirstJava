94a96d32e3c4629e665449f2b984a06a730c4dbe
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-07 15:41:16 +0400

    Вариант из книжки

--- src/com/firstjava/LessFromPage53_PoolPuzzleOne.java
+++ src/com/firstjava/LessFromPage53_PoolPuzzleOne.java
@@ -4,17 +4,21 @@ public class LessFromPage53_PoolPuzzleOne {
     public static void main(String[] args) {
         int x = 0;
         while (x < 4) {
+            System.out.print("a");
             if (x < 1) {
-                System.out.print("a ");
-                System.out.print("noise");
+                System.out.print(" ");
             }
+            System.out.print("n");
+
             if (x > 1) {
-                System.out.print("an");
                 System.out.print(" oyster");
                 x = x + 2;
             }
             if (x == 1) {
-                System.out.print("annoys");
+                System.out.print("noys");
+            }
+            if (x < 1) {
+                System.out.print("oise");
             }
             System.out.println();
             x = x + 1;
